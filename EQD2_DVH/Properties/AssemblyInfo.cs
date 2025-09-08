a70afb0b044b861c6705c6d2d484104cecdb4ec7
RisDicom <risto.hirvilammi@gmail.com>
2025-09-08 15:07:01 +0300

    Update AssemblyInfo.cs
    Poistettu turhia kommentteja

--- EQD2_DVH/Properties/AssemblyInfo.cs
+++ EQD2_DVH/Properties/AssemblyInfo.cs
@@ -2,13 +2,12 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Tämä on tärkein rivi täällä
 [assembly: ESAPIScript(IsWriteable = false)]
 
 // Muut tiedot voivat olla oletusarvoisia
 [assembly: AssemblyTitle("EQD2_DVH")]
 [assembly: AssemblyProduct("EQD2_DVH")]
 [assembly: ComVisible(false)]
-[assembly: Guid("814C8D26-0C88-44ED-86AE-9518726C034A")] // Voit luoda uuden GUID:n (Tools -> Create GUID)
+[assembly: Guid("814C8D26-0C88-44ED-86AE-9518726C034A")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
